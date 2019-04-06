6f19ca6bf2fc641b0a2a1dc27a864db2a3c00d6c
manueltrujillo02 <46513824+manueltrujillo02@users.noreply.github.com>
2019-04-05 18:59:02 -0700

    Add files via upload

---
+++ seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("MC Delight", FALSE);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Torcado",FALSE);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("MC Pine",FALSE);
